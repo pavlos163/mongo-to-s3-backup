a3bd8a81bec669e15be9b05161fe9413570c9335
Pavlos Kosmetatos <pavlos163@gmail.com>
2024-03-23 13:46:45 +0200

    Gzip the file

--- mongo-dump.sh
+++ mongo-dump.sh
@@ -2,5 +2,5 @@
 OBJECT_NAME=$(date '+%Y-%m-%d')
 
 # Dump and stream directly to S3
-mongodump --uri="$MONGO_URI" --archive | \
+mongodump --uri="$MONGO_URI" --archive --gzip | \
 aws s3 cp - s3://"$BUCKET_NAME"/"$OBJECT_NAME" --endpoint-url "$CLOUDFLARE_ENDPOINT"
